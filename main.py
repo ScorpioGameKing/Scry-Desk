18b1601b9b75d34b2a8ec2f2965713f38df9e47b
ScorpioGK <scorpiogameking@gmail.com>
2026-03-25 20:51:59 -0400

    cleared cache and added to ignore, testing searches for file struct

--- main.py
+++ main.py
@@ -23,15 +23,17 @@ slice_types = [
 matched_cards = {}
 
 # Tested Updating, Slicing and Loading Methods
-#data_manager.update_sliced_card_data(True)
-data_manager.load_slices_to_memory(slice_types)
+#data_manager.update_sliced_card_data()
+data_manager.load_slices_to_memory(['Land'])
 
 # Tested Slice Soft Search Methods
-#matched_cards = data_manager.soft_search_loaded_slices_for_name('Forest')
 #matched_cards = data_manager.soft_search_loaded_slices_for_color_identity(["B"])
 #matched_cards = data_manager.soft_search_loaded_slices_for_colors(['B'])
 
 # Testing Methods
+matched_cards = data_manager.soft_search_loaded_slices_for_name('Forest')
+with open('card_data/saved_searches/forest_cards.json', 'w') as saved_search:
+    json.dump(matched_cards, saved_search)
 
 #for card in matched_cards:
 #    print(card, len(matched_cards[card]))
